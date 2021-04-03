805de6df9cef00a33bc6a4e0448433881a385bba
student <cbames07@gmail.com>
2021-04-03 04:04:04 +0000

    Initial commit.

---
+++ src/main/java/caltech/jenkins/jenkinsassessment/CaltechJenkinsAssessmentApplication.java
@@ -0,0 +1,13 @@
+package caltech.jenkins.jenkinsassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaltechJenkinsAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaltechJenkinsAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/caltech/jenkins/jenkinsassessment/CaltechJenkinsAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package caltech.jenkins.jenkinsassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaltechJenkinsAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
